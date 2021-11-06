5579e3de28ed5171c1c93fa941906acf368a52e1
Maara12 <maaranshepard@gmail.com>
2021-11-06 18:23:52 +0530

    Rotating Movement

---
+++ CutGrass/Assets/Scripts/AlternatingRotation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlternatingRotation : MonoBehaviour
+{
+    public Transform BallA;
+    public Transform BallB;
+    public Transform Connector;
+
+    bool isParentBallA = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //change parent ball
+            if(isParentBallA)
+            {
+                Debug.Log("inputReceived");
+                ChangeParentBall(BallA,Connector,BallB);
+                isParentBallA = false;
+            }
+            else
+            {
+                ChangeParentBall(BallB, Connector, BallA);
+                isParentBallA = true;
+            }
+            
+        }
+    }
+
+    private void ChangeParentBall(Transform parentball, Transform connector, Transform childball)
+    {
+        parentball.DetachChildren();
+        parentball.parent = childball;
+        connector.parent = childball;
+        
+    }
+}
---
+++ CutGrass/Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float RotationModifier = 200f;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotates the Ball
+        gameObject.transform.Rotate(0f, RotationModifier * Time.deltaTime, 0f, Space.Self);
+    }
+}
